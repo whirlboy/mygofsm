a532633331b7909017caf7f8f590ea01a27db3bf
whirlboy <yzc99660@163.com>
2022-04-10 23:34:40 +0800

    update gofsm

--- order_transition_demo/order_flow_test.go
+++ order_transition_demo/order_flow_test.go
@@ -11,13 +11,13 @@ func TestFSM(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 
 	statusFlow := &StatusFlow{
-		ID:                88888888,
-		StatusCore:        s1,
-		HistoryStatusCore: []StatusCore{s1},
+		ID:                 88888888,
+		StatusGroup:        s1,
+		HistoryStatusGroup: []StatusGroup{s1},
 	}
 	fmt.Printf("老状态:%v\n", statusFlow)
 	fsm := initFSM()
-	err := fsm.Trigger(statusFlow.StatusCore, "demander_approve_script", statusFlow)
+	err := fsm.Trigger(statusFlow.StatusGroup, "demander_approve_script", statusFlow)
 	if err != nil {
 		t.Errorf("trigger err: %v", err)
 	}
